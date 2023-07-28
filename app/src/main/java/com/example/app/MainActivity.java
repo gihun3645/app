3c153beb8752d8ee849e1c2ba4c38fc2af30d237
송바래(ばれ） <gihun3645@naver.com>
2023-07-28 14:05:54 +0900

    안드로이드 앱 만들기 #8 (SharedPreferences)

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,15 +12,31 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText et_save;
+    String shared = "file";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nav);
+        setContentView(R.layout.activity_main);
 
+        et_save = (EditText)findViewById(R.id.et_save);
 
-        // Nav 클래스 시작
-        Intent intent = new Intent(this, Nav.class);
-        startActivity(intent);
+        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
+        String value = sharedPreferences.getString("송바래", "");
+        et_save.setText(value);
+    }
+
+    // 생명주기를 하나 더 생성
+    // 액티비티가 종료될 때 실행하는 것
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        String value = et_save.getText().toString();
+        editor.putString("송바래", value);
+        editor.commit();
     }
 }
