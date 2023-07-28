77568eab72c0a5ad6b0f7f229f342ce1bfc92a4f
송바래(ばれ） <gihun3645@naver.com>
2023-07-28 12:32:27 +0900

    안드로이드 앱 만들기 #6 listview

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,46 +2,32 @@ package com.example.app;
 
 import android.content.Intent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private EditText et_text;
-    private Button btn_move;
-    private String str;
-    ImageView test;
+
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        list = (ListView) findViewById(R.id.list);
 
-        et_text = findViewById(R.id.et_text);
+        List<String> data = new ArrayList<>();
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
 
-        btn_move = findViewById(R.id.btn_move);
-        btn_move.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                str = et_text.getText().toString();
-                Intent intent = new Intent(MainActivity.this, SubActivity.class);
-                intent.putExtra("str", str); // str이라는 이름으로 str값을 넘겨줌 (키, 값)
-                startActivity(intent); // 액티비티 이동
-            }
-        });
-
-        test = findViewById(R.id.test);
-        test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "송바래의 개발일상", Toast.LENGTH_SHORT).show();
-            }
-        });
+        data.add("Hello World!");
+        data.add("this is app");
+        adapter.notifyDataSetChanged();
     }
 }
